72bb4e307fe7faa7dbd7722671d8c10ae55716d1
Lucas Monstro <joao.galiano.silva@gmail.com>
2023-11-06 07:52:30 +0000

    refactor: improve speed

--- dxcam/processor/numpy_processor.py
+++ dxcam/processor/numpy_processor.py
@@ -1,5 +1,6 @@
 import ctypes
-import numpy as np
+from numpy import rot90, ndarray, newaxis, uint8, zeros
+from numpy.ctypeslib import as_array
 from .base import Processor
 
 
@@ -7,8 +8,7 @@ class NumpyProcessor(Processor):
     def __init__(self, color_mode):
         self.cvtcolor = None
         self.color_mode = color_mode
-        if self.color_mode=='BGRA':
-            self.color_mode = None
+        self.PBYTE = ctypes.POINTER(ctypes.c_ubyte)
 
     def process_cvtcolor(self, image):
         import cv2
@@ -19,56 +19,42 @@ class NumpyProcessor(Processor):
                 "RGB": cv2.COLOR_BGRA2RGB,
                 "RGBA": cv2.COLOR_BGRA2RGBA,
                 "BGR": cv2.COLOR_BGRA2BGR,
-                "GRAY": cv2.COLOR_BGRA2GRAY
+                "GRAY": cv2.COLOR_BGRA2GRAY,
+                "BGRA": None,
             }
             cv2_code = color_mapping[self.color_mode]
-            if cv2_code != cv2.COLOR_BGRA2GRAY:
-                self.cvtcolor = lambda image: cv2.cvtColor(image, cv2_code)
+            if cv2_code is not None:
+                if cv2_code != cv2.COLOR_BGRA2GRAY:
+                    self.cvtcolor = lambda image: cv2.cvtColor(image, cv2_code)
+                else:
+                    self.cvtcolor = lambda image: cv2.cvtColor(image, cv2_code)[
+                        ..., newaxis
+                    ]
             else:
-                self.cvtcolor = lambda image: cv2.cvtColor(image, cv2_code)[
-                    ..., np.newaxis
-                ] 
+                return image
+
         return self.cvtcolor(image)
 
+    def shot(self, image_ptr, rect, width, height):
+        ctypes.memmove(image_ptr, rect.pBits, height*width*4)
+
     def process(self, rect, width, height, region, rotation_angle):
-        pitch = int(rect.Pitch)
+        width = region[2] - region[0]
+        height = region[3] - region[1]
+        if rotation_angle in (90, 270):
+            width, height = height, width
 
-        if rotation_angle in (0, 180):
-            offset = (region[1] if rotation_angle==0 else height-region[3])*pitch
-            height = region[3] - region[1]
-        else:
-            offset = (region[0] if rotation_angle==270 else width-region[2])*pitch
-            width = region[2] - region[0]
+        buffer = ctypes.cast(rect.pBits, self.PBYTE)
+        image = as_array(buffer, (height, width, 4))
 
-        if rotation_angle in (0, 180):
-            size = pitch * height
-        else:
-            size = pitch * width
+        # Another approach from https://github.com/Agade09/DXcam
+        # buffer = (ctypes.c_char*height*width*4).from_address(ctypes.addressof(rect.pBits.contents))
+        # image = ndarray((height, width, 4), dtype=uint8, buffer=buffer)
 
-        pitch = pitch // 4
-        if rotation_angle in (0, 180):
-            image = np.ndarray((height, pitch, 4), dtype=np.uint8, buffer=(ctypes.c_char*size).from_address(ctypes.addressof(rect.pBits.contents)+offset))
-        elif rotation_angle in (90, 270):
-            image = np.ndarray((width, pitch, 4), dtype=np.uint8, buffer=(ctypes.c_char*size).from_address(ctypes.addressof(rect.pBits.contents)+offset))
+        if rotation_angle != 0:
+            image = rot90(image, k=rotation_angle//90, axes=(1, 0))
 
         if self.color_mode is not None:
-            image = self.process_cvtcolor(image)
-
-        if rotation_angle == 90:
-            image = np.rot90(image, axes=(1, 0))
-        elif rotation_angle == 180:
-            image = np.rot90(image, k=2, axes=(0, 1))
-        elif rotation_angle == 270:
-            image = np.rot90(image, axes=(0, 1))
-
-        if rotation_angle in (0, 180) and pitch != width:
-            image = image[:, :width, :]
-        elif rotation_angle in (90, 270) and pitch != height:
-            image = image[:height, :, :]
-
-        if region[3] - region[1] != image.shape[0]:
-            image = image[region[1] : region[3], :, :]
-        if region[2] - region[0] != image.shape[1]:
-            image = image[:, region[0] : region[2], :]
+            return self.process_cvtcolor(image)
 
         return image
